f988d592457298b73fce843f5794d4d8fab549fd
jenildhorajiya <jenild2002@gmail.com>
2022-07-31 15:38:00 +0530

    final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,6 +33,26 @@ function Home() {
         leftBtnText="Custom order"
         rightBtnText="Existing Inventory"
       />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      ></Section>
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+      ></Section>
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      ></Section>
     </Container>
   );
 }
